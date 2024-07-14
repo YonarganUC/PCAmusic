2197345ba3f09f03c08f1995bf9e5f83f7365509
YonarganUC <72508579@continental.edu.pe>
2024-07-14 17:54:42 -0500

    Tema 3: Slides dimanicos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Icon } from 'ionicons/dist/types/components/icon/icon';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 
 export class HomePage {
+
+  slides = [
+    {
+      title: "Titulo de slide 1",
+      icon: "aperture",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFJfQpO3v4NSrlVvMpFYWw7YjijzAKTbuXHg&s",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8L9JE5eek90hEgXgfxJui1x5c1f3aD4nGVA&s",
+      description: "otro gato"
+    },
+    {
+      title: "Titulo de slide 2",
+      icon: "aperture",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFJfQpO3v4NSrlVvMpFYWw7YjijzAKTbuXHg&s",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8L9JE5eek90hEgXgfxJui1x5c1f3aD4nGVA&s",
+      description: "otro gato"
+    },
+    {
+      title: "Titulo de slide 3",
+      icon: "aperture",
+      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFJfQpO3v4NSrlVvMpFYWw7YjijzAKTbuXHg&s",
+      image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8L9JE5eek90hEgXgfxJui1x5c1f3aD4nGVA&s",
+      description: "otro gato"
+    }
+  ]
 
   constructor() {}
 
