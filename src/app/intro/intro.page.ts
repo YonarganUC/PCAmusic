4b851c4a7ed8c2f4c8babc7c932aaff1d964af2e
YonarganUC <72508579@continental.edu.pe>
2024-07-15 15:54:09 -0500

    Tema 5: Ionic Storage

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-intro',
@@ -47,13 +48,19 @@ export class IntroPage implements OnInit {
     }
   ]
 
-  constructor(private router: Router) { }
+  constructor(
+
+    private router: Router,
+    private storage: Storage
+
+  ) { }
 
   ngOnInit() {
   }
 
   close() {
     //console.log("estoy intentando cerrar la intro")
+    this.storage.set("isIntroShowed", true)
     this.router.navigateByUrl("/home")
   }
 
