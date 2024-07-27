4518802e2db7da87a8a25b6431597df5190992c7
YonarganUC <72508579@continental.edu.pe>
2024-07-26 19:02:36 -0500

    Tema 8: Formularios Reactivos Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup
+
+  constructor(
+    private formBuilder:FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.email
+        ])
+      )
+    })
+  }
 
   ngOnInit() {
+  }
+
+  loginUser(datalogin:any) {
+    console.log(datalogin)
   }
 
 }
