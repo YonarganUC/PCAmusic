20e098e12067732a7dcd6ba1d39235484c83ef52
YonarganUC <72508579@continental.edu.pe>
2024-08-01 11:45:57 -0500

    Tema 14: Componente menu

---
+++ src/app/menu/menu.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController, NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.page.html',
+  styleUrls: ['./menu.page.scss'],
+})
+export class MenuPage implements OnInit {
+
+  constructor(
+    private menu: MenuController,
+    private navCtrl: NavController
+  ) { }
+
+  ngOnInit() {
+  }
+
+  closeMenu() {
+    console.log("cerrar menu")
+    this.menu.close()
+  }
+
+  logout() {
+    console.log("cerrar sesión")
+    this.navCtrl.navigateRoot("/login")
+  }
+}
